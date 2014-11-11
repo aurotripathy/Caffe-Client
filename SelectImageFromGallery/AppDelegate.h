3fb57c40e0782f84c67c8bd68209e29934fd1652
Vipin <Vipin@Vipins-Mac-mini.local>
2014-11-11 22:00:48 +0530

    Initial Commit

---
+++ SelectImageFromGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectImageFromGallery
+//
+//  Created by Vipin on 11/11/14.
+//  Copyright (c) 2014 CoderzHeaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectImageFromGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectImageFromGallery
+//
+//  Created by Vipin on 11/11/14.
+//  Copyright (c) 2014 CoderzHeaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
